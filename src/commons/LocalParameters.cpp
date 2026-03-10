8dc75c74ad0eddab73cfd905963d13bf74dc012b
Sooyoung Cha <ellen2g77@gmail.com>
2026-03-10 15:44:49 +0900

    Set chain tm default paramter to 0

--- src/commons/LocalParameters.cpp
+++ src/commons/LocalParameters.cpp
@@ -357,7 +357,7 @@ LocalParameters::LocalParameters() :
     dbExtractionMode = DB_EXTRACT_MODE_CHAIN;
     distanceThreshold = 10.0;
     filtMultTmThr = 0.0;
-    filtChainTmThr = 0.3;
+    filtChainTmThr = 0.0;
     filtInterfaceLddtThr = 0;
     minAlignedChains = 2;
 
